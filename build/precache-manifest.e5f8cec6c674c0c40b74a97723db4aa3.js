71fa2cfc36909c3e4e895cd3db3ad3613ab79f4c
MostafaAmrHamed <mostafaamro5619@gmail.com>
2020-10-18 01:02:24 +0200

    added production build

---
+++ build/precache-manifest.e5f8cec6c674c0c40b74a97723db4aa3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "636e6136684705c6552c7a68d64636ab",
+    "url": "./index.html"
+  },
+  {
+    "revision": "75e2dab05b01e7431aab",
+    "url": "./static/css/2.ceb79488.chunk.css"
+  },
+  {
+    "revision": "f0cd8fdfbbede8070bd3",
+    "url": "./static/css/main.33c1b24b.chunk.css"
+  },
+  {
+    "revision": "75e2dab05b01e7431aab",
+    "url": "./static/js/2.1b3efd1e.chunk.js"
+  },
+  {
+    "revision": "c3f4a91b08326551c3d2c23a3296dfa8",
+    "url": "./static/js/2.1b3efd1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0cd8fdfbbede8070bd3",
+    "url": "./static/js/main.159de855.chunk.js"
+  },
+  {
+    "revision": "b2304df7646f9331ac36",
+    "url": "./static/js/runtime-main.bf3edb8d.js"
+  }
+]);
